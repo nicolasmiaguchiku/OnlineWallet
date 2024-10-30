9698a5316f8e4e226d19bb762cd57b52909cc2e8
nicolasmiaguchiku <nicolaseesiuke@gmail.com>
2024-10-29 23:00:41 -0300

    Preparing environment

---
+++ OnlineWallet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineWallet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
