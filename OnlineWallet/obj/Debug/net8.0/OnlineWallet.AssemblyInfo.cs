534481b7785a875439cc13e0512b3807758131f2
nicolasmiaguchiku <nicolaseesiuke@gmail.com>
2024-12-13 00:23:26 -0300

    reparing commit

--- OnlineWallet/obj/Debug/net8.0/OnlineWallet.AssemblyInfo.cs
+++ OnlineWallet/obj/Debug/net8.0/OnlineWallet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineWallet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e5644b843e7c3fafe18a20e6c9678ca6d57160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c8349582937aa16c30587be16ca1afb3c9dcd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineWallet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineWallet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
